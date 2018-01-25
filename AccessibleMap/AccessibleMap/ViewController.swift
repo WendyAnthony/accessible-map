6a0916aac747edb0b5a062ed114bb5ef5c701181
Mark Dostal <mhdostal@aol.com>
2018-01-25 16:46:58 -0600

    initial routing code

--- AccessibleMap/AccessibleMap/ViewController.swift
+++ AccessibleMap/AccessibleMap/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
     private var map: AGSMap!
     @IBOutlet var zoomInButton: UIButton!
     @IBOutlet var zoomOutButton: UIButton!
+    
+    var routeTask:AGSRouteTask = AGSRouteTask(url: URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NetworkAnalysis/SanDiego/NAServer/Route")!)
+    var routeParameters:AGSRouteParameters?
+    var generatedRoute:AGSRoute?
+    var stopGraphicsOverlay = AGSGraphicsOverlay()
+    var routeGraphicsOverlay = AGSGraphicsOverlay()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -49,7 +56,10 @@ class ViewController: UIViewController {
         
         // zoom to user location
         startLocationDisplay()
-    }
+        
+        //add graphicsOverlays to the map view
+        self.mapView.graphicsOverlays.addObjects(from: [routeGraphicsOverlay, stopGraphicsOverlay])
+}
 
     func startLocationDisplay() {
         mapView.locationDisplay.autoPanMode = .recenter
@@ -91,6 +101,75 @@ class ViewController: UIViewController {
         let zoomedExtent = currentExtent.toBuilder().expand(byFactor: 0.5).toGeometry()
         let viewpoint = AGSViewpoint(targetExtent: zoomedExtent)
         mapView.setViewpoint(viewpoint, duration: 1.5, completion: nil)
+    }
+    
+    // MARK: Routing
+    
+    //
+    // route - handles routing from a starting location to an ending location
+    // will display the route on the map
+    // will speak the text directions when the routing completes
+    //
+    func route(_ startLocation: AGSPoint, endLocation: AGSPoint) {
+        //route only if default parameters are fetched successfully
+        guard let parameters = routeParameters else {
+            print("Default route parameters not loaded")
+            return
+        }
+        
+        //set parameters to return directions
+        parameters.returnDirections = true
+        
+        //clear previous routes
+        routeGraphicsOverlay.graphics.removeAllObjects()
+        
+        //clear previous stops
+        parameters.clearStops()
+        
+        //set the stops
+        let stop1 = AGSStop(point: startLocation)
+        stop1.name = "Origin"
+        let stop2 = AGSStop(point: endLocation)
+        stop2.name = "Destination"
+        parameters.setStops([stop1, stop2])
+        
+        routeTask.solveRoute(with: parameters) { [weak self] (routeResult: AGSRouteResult?, error: Error?) -> Void in
+            if let error = error {
+                print(error)
+            }
+            else {
+                //show the resulting route on the map
+                //also save a reference to the route object
+                //in order to access directions
+                guard let route = routeResult?.routes.first else {
+                    print("No route")
+                    return
+                }
+                
+                self?.generatedRoute = route
+                let routeGraphic = AGSGraphic(geometry: route.routeGeometry, symbol: self?.routeSymbol(), attributes: nil)
+                self?.routeGraphicsOverlay.graphics.add(routeGraphic)
+            }
+        }
+    }
+    
+    //method to get the default parameters for the route task
+    func getDefaultParameters() {
+        routeTask.defaultRouteParameters { [weak self] (params: AGSRouteParameters?, error: Error?) -> Void in
+            if let error = error {
+                print(error)
+            }
+            else {
+                //on completion store the parameters
+                self?.routeParameters = params
+            }
+        }
+    }
+    
+    // method provides a line symbol for the route graphic
+    func routeSymbol() -> AGSSimpleLineSymbol {
+        let symbol = AGSSimpleLineSymbol(style: .solid, color: UIColor.yellow, width: 5)
+        return symbol
     }
 }
 
