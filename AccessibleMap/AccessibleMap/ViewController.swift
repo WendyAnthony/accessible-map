605ce91e1bb4f7232a12b3532427648957068ad4
Mark Dostal <mhdostal@aol.com>
2018-01-25 15:54:18 -0600

    implement zoom in/out buttons

--- AccessibleMap/AccessibleMap/ViewController.swift
+++ AccessibleMap/AccessibleMap/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: AGSMapView!
     private var map: AGSMap!
-    
+    @IBOutlet var zoomInButton: UIButton!
+    @IBOutlet var zoomOutButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,6 +26,9 @@ class ViewController: UIViewController {
         // accessibility
         mapView.isAccessibilityElement = true
         mapView.accessibilityLanguage = "en-US"
+        
+        // turn off pan/zoom/rotate gestures
+        mapView.interactionOptions.isEnabled = false
         
         // activate voiceover when zoom in/out completes
         mapView.viewpointChangedHandler = { [weak self] () in
@@ -63,6 +67,30 @@ class ViewController: UIViewController {
         // ref. https://community.esri.com/thread/118146
         let zoom = log(591657550.500000 / (mapScale / 2)) / log(2.0)
         return Int(zoom)
+    }
+    
+    // MARK: UI Actions
+    
+    @IBAction func zoomOut(_ sender: Any) {
+        guard let currentExtent = mapView.currentViewpoint(with: .boundingGeometry)?.targetGeometry as? AGSEnvelope else {
+            //no viewpoint extent yet
+            return
+        }
+        
+        let zoomedExtent = currentExtent.toBuilder().expand(byFactor: 2.0).toGeometry()
+        let viewpoint = AGSViewpoint(targetExtent: zoomedExtent)
+        mapView.setViewpoint(viewpoint, duration: 1.5, completion: nil)
+    }
+    
+    @IBAction func zoomIn(_ sender: Any) {
+        guard let currentExtent = mapView.currentViewpoint(with: .boundingGeometry)?.targetGeometry as? AGSEnvelope else {
+            //no viewpoint extent
+            return
+        }
+        
+        let zoomedExtent = currentExtent.toBuilder().expand(byFactor: 0.5).toGeometry()
+        let viewpoint = AGSViewpoint(targetExtent: zoomedExtent)
+        mapView.setViewpoint(viewpoint, duration: 1.5, completion: nil)
     }
 }
 
